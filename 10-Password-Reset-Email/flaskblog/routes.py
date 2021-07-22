1e40d2827b44dfbec3eff791752ef99f0a65fd96
sabita rajbanshi <sabitarajbanshi.tech@gmail.com>
2021-07-23 00:12:29 +0545

    Flask

--- 10-Password-Reset-Email/flaskblog/routes.py
+++ 10-Password-Reset-Email/flaskblog/routes.py
@@ -160,7 +160,7 @@ def user_posts(username):
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request',
-                  sender='sabitarajbanshi.tech@gmail.com',
+                  sender='noreply@demo.com',
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('reset_token', token=token, _external=True)}
